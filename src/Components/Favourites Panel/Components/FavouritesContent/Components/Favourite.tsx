9b6cbf8e6fb37b8c1dc7ac94d849fa835a089ec4
David B <59053870+David-Beale@users.noreply.github.com>
2021-11-30 20:21:57 +0000

    add favourites onclick

--- src/Components/Favourites Panel/Components/FavouritesContent/Components/Favourite.tsx
+++ src/Components/Favourites Panel/Components/FavouritesContent/Components/Favourite.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { EntityId } from "@reduxjs/toolkit";
 import {
   removeFavourite,
@@ -6,24 +7,43 @@ import {
 import { useAppDispatch, useAppSelector } from "../../../../../redux/hooks";
 import { Title } from "../../../../Content/Components/Row/Card/CardStyle";
 import { FavouriteContainer, RemoveButton } from "./FavouriteStyle";
+import { setDetailsId } from "../../../../../redux/details";
 
 interface IProps {
   favouriteId: EntityId;
 }
 
 export default function Favourite({ favouriteId }: IProps) {
+  const titleRef = useRef<HTMLElement>(null);
   const dispatch = useAppDispatch();
   const favourite = useAppSelector((state) =>
     selectFavouriteById(state, favouriteId)
   );
-  const onClick = () => {
+  const onRemove = () => {
     dispatch(removeFavourite(favouriteId));
   };
+
+  const onTitleClick = () => {
+    if (!favouriteId || !titleRef.current) return;
+    const rect = titleRef.current.getBoundingClientRect();
+    const midHorizontal = rect.left + rect.width / 2;
+    const midVertical = rect.top + rect.height / 2;
+
+    dispatch(
+      setDetailsId({
+        id: favouriteId,
+        position: `${midHorizontal}px ${midVertical}px`,
+      })
+    );
+  };
   if (!favourite) return null;
+
   return (
     <FavouriteContainer>
-      <Title>{favourite.title}</Title>
-      <RemoveButton onClick={onClick} />
+      <Title onClick={onTitleClick} ref={titleRef}>
+        {favourite.title}
+      </Title>
+      <RemoveButton onClick={onRemove} />
     </FavouriteContainer>
   );
 }
